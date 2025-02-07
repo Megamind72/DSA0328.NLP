c5119814ec545c196dd46add31e1fc0e054be82d
Amritesh prakash <112755378+Megamind72@users.noreply.github.com>
2025-02-07 09:11:57 +0530

    Add files via upload

--- NLP_06_txt_using_bigram_model.py
+++ NLP_06_txt_using_bigram_model.py
@@ -3,21 +3,16 @@ import random
 from nltk.util import bigrams
 from collections import defaultdict
 
-# Sample text for training the bigram model
 text = "the cat sat on the mat the cat ate a rat the rat ran away"
 
-# Tokenization
 words = text.split()
 
-# Generate bigrams
 bigram_pairs = list(bigrams(words))
 
-# Create a dictionary to store bigram probabilities
 bigram_model = defaultdict(list)
 for w1, w2 in bigram_pairs:
     bigram_model[w1].append(w2)
 
-# Function to generate text using bigram model
 def generate_text(start_word, length=10):
     current_word = start_word
     sentence = [current_word]
